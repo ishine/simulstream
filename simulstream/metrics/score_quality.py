bcc8b78b65325bb807562a555cd611c8cbfe109d
Marco Gaido <mgaido@fbk.eu>
2025-10-15 16:29:29 +0200

    fix logging overriding mweralign format

--- simulstream/metrics/score_quality.py
+++ simulstream/metrics/score_quality.py
@@ -26,6 +26,7 @@ logging.basicConfig(
     format='%(asctime)s | %(levelname)s | %(name)s | %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.INFO,
+    force=True
 )
 LOGGER = logging.getLogger('simulstream.score_quality')
 
