8cf2a41ca582c3b8326c005bb8199e2dd839c070
Marco Gaido <mgaido@fbk.eu>
2026-01-23 08:37:43 +0100

    [INSTALL][BUG] Fix editable install in a clean env in getting simulstream version (#12)
    # Why is needed?
    
    When running an editable install in a clean enviroment (e.g. in the
    creation of a Docker image), the installation fails because it tried to
    resolve the project version before the installation of the simulstream
    package, so the `simulstream.__version__` is not yet there.
    
    # What does the PR do?
    
    Keep the version in a txt file.
    
    # How is this documented?
    
    NA
    
    # How was the PR tested?
    
    Creation of a Docker image.

--- simulstream/__init__.py
+++ simulstream/__init__.py
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
-__version__ = '0.2.0'
+from pathlib import Path
+
+
+with Path(__file__).with_name('VERSION.txt').open('r') as f:
+    __version__ = f.read().strip()
